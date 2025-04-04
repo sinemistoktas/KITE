9e80a7507dd46fc2c2d4e0b3f1954b1971240197
makca21 <91735737+makca21@users.noreply.github.com>
2025-04-04 20:12:15 +0300

    Add files via upload

---
+++ data/data_segmentation.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data, filters, morphology, restoration, transform, registration, exposure, feature
+from scipy.ndimage import map_coordinates, binary_fill_holes, median_filter, gaussian_laplace, uniform_filter, generic_filter, gaussian_filter, laplace
+from skimage.io import imread
+from scipy.stats import trim_mean
+import cv2
+import os
+import pandas as pd
+import json
+from data_preprocessing import Preprocessor
+# steps to follow
+# 1. load the data
+# 2. preprocess the data
+# 3. find the ground truth
+# 4. make annotation 
+# 5. save the annotated data
+# 6. start segmentation based on the annotated data
+# 7. save the segmented data
+
+# subject 09_31 , 10_33
+
+class SegmentationModel():
+    def __init__(self,preprocessor):
+        self.preprocessor = preprocessor
+        self.input_directory = "./duke_original/image"
+        self.ground_truth_directory = "./duke_original/lesion"
+    
+    def grow_region(self,image, seed_mask, threshold=10):
+        height, width = image.shape
+        grown_mask = seed_mask.copy()
+        seeds = list(zip(*np.nonzero(seed_mask)))
+        visited = set(seeds)
+
+        while seeds:
+            x, y = seeds.pop()
+            for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]: # 8-connectivity
+                nx, ny = x + dx, y + dy
+                if (0 <= nx < height) and (0 <= ny < width):
+                    if (nx, ny) in visited:
+                        continue
+
+                    visited.add((nx, ny))
+                    if grown_mask[nx, ny] == 0:
+                        if abs(int(image[nx, ny]) - int(image[x, y])) < threshold:
+                            grown_mask[nx, ny] = 1
+                            seeds.append((nx, ny))
+        return grown_mask
+
+
+
+#input_directory = "./duke_original/image"
+#ground_truth_directory = "./duke_original/lesion"
+preprocessor = Preprocessor()
+segmentation_model = SegmentationModel(preprocessor)
+filename = "Subject_10_33.png"
+image_path = os.path.join(preprocessor.input_directory, filename)
+image = imread(image_path, as_gray= True)
+ground_truth = preprocessor.find_ground_truth(filename, image)
+
+image = preprocessor.preprocess_image(image)
+bg_removed = preprocessor.thresholding(image, 0.4)
+bg_removed = morphology.closing(bg_removed, morphology.square(8))
+initial_mask = (bg_removed > 0).astype(np.uint8)
+gray_image =  (image * 255).astype(np.uint8)
+grown = segmentation_model.grow_region(gray_image, initial_mask, threshold=1)
+
+#image_rgb = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2RGB)
+#plt.imshow(cv2.addWeighted(image_rgb, 0.5, cv2.cvtColor(grown, cv2.COLOR_GRAY2RGB), 0.5, 0))
+#plt.imshow(grown, cmap="gray")
+
+image_rgb = np.stack([gray_image]*3, axis=-1).astype(np.uint8)
+image_rgb[grown == 1] = [0, 0, 255]
+
+with open("Subject_10_33.json") as f:
+    data = json.load(f)
+
+for shape in data['shapes']:
+    label = shape['label']
+    points = shape['points']  # List of (x, y) coordinates
+    print(f"{label}: {points}")
+
+points = np.array(points, dtype=np.int32)
+seed_mask = np.zeros(image.shape, dtype=np.uint8)
+cv2.fillPoly(seed_mask, [points], 1)  # Fill the triangle area with 1s
+grown_mask = segmentation_model.grow_region(gray_image, seed_mask, threshold=5)
+# reduce the noise
+grown_mask = morphology.closing(grown_mask, morphology.square(4))
+image_rgb = np.stack([gray_image]*3, axis=-1).astype(np.uint8)
+image_rgb[grown_mask== 1] = [0, 0, 255]
+# show with ground truth
+ground_truth = preprocessor.find_ground_truth(filename, image)
+
+# plot all
+plt.figure(figsize=(12, 6))
+
+# 1. Original Image
+plt.subplot(1, 4, 1)
+plt.imshow(image, cmap='gray')
+plt.title("Original")
+plt.axis('off')
+
+# 2. Thresholded
+plt.subplot(1, 4, 2)
+plt.imshow(bg_removed, cmap='gray')
+plt.title("Thresholded")
+plt.axis('off')
+
+# 3. Region Grown Overlay (Blue)
+plt.subplot(1, 4, 3)
+plt.imshow(image_rgb)
+plt.title("Segmented region highlighted")
+plt.axis('off')
+
+# 4. Ground Truth Overlay
+plt.subplot(1, 4, 4)
+plt.imshow(gray_image, cmap='gray')
+plt.contour(ground_truth, colors='red', linewidths=1)
+plt.title("With Ground Truth")
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
