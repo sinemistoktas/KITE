f0f5e780ddadb543da2a9269d9f082f5bdc5788f
Sinemis Toktaş <102419519+sinemistoktas@users.noreply.github.com>
2025-05-18 21:59:02 +0300

    added segmentation algorithm selection choice to UI

--- website/frontend/views.py
+++ website/frontend/views.py
@@ -4,16 +4,12 @@ from django.core.files.storage import FileSystemStorage
 from django.http import JsonResponse, HttpResponse
 from data.data_preprocessing import Preprocessor
 from data.data_segmentation import SegmentationModel
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib
 matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 import json
 import io
 import os
 from skimage.io import imread
-from skimage.util import img_as_ubyte
-from skimage import morphology
 from base64 import b64encode
 from PIL import Image
 import traceback
@@ -29,16 +25,20 @@ def home(request):
 def seg_tool(request):
     image_url = None
     image_name = None
+    algorithm_name = None
 
     if request.method == 'POST' and request.FILES.get('image'):
         image = request.FILES['image']
         image_name = image.name
+        algorithm = request.POST.get('algorithm')
+        algorithm_name = dict(request.POST).get('algorithm_name', [''])[0]
         fs = FileSystemStorage()
         filename = fs.save(image_name, image)
         image_url = fs.url(filename)
     return render(request, 'pages/segtool.html', {
         'image_url': image_url,
-        'image_name': image_name
+        'image_name': image_name,
+        'algorithm_name': algorithm_name
     })
 
 # Takes an HTTP request, which must include the name of the image uploaded, and the json formatted annotated file.
@@ -49,30 +49,56 @@ def segment_image(request):
         try:
             data = json.loads(request.body)
 
-            for shape in data["shapes"]:
-                points = shape.get("points", [])
-            #print("Data", data) // for debugging
-           
+            # Determine selected algorithm
+            algorithm = data.get("algorithm", "kite")
+
+            # Get filename
             filename = data.get("image_name")
             if not filename:
                 return JsonResponse({"error": "No image name provided."}, status=400)
+
+            # Get file
             image_path = os.path.join(settings.MEDIA_ROOT, filename)
             if not os.path.exists(image_path):
                 return JsonResponse({"error": "Image file not found."}, status=404)
-            image = imread(image_path, as_gray= True)
-            result_img = segmentation_model.run_segmentation_from_json_without_ground_truth(image, data)
-            predicted_points = segmentation_model.get_predicted_points()
 
-            # Ensures that the original image is returned when there are no annotations.
-            buf = io.BytesIO()
-            result_img.save(buf, format="PNG")
-            buf.seek(0)
-            encoded_image = b64encode(buf.getvalue()).decode("utf-8")
+            # Run the selected algorithm
+            ## KITE (our algorithm)
+            if algorithm == "kite":
+                for shape in data["shapes"]:
+                    points = shape.get("points", [])
+                #print("Data", data) // for debugging
 
-            return JsonResponse({
-                "segmented_image": encoded_image, # will be the original image if there are no annotations
-                "predicted_annotations": predicted_points,  # will be [] if no annotations
-            })
+                image = imread(image_path, as_gray= True)
+                result_img = segmentation_model.run_segmentation_from_json_without_ground_truth(image, data)
+                predicted_points = segmentation_model.get_predicted_points()
+
+                # Ensures that the original image is returned when there are no annotations.
+                buf = io.BytesIO()
+                result_img.save(buf, format="PNG")
+                buf.seek(0)
+                encoded_image = b64encode(buf.getvalue()).decode("utf-8")
+
+                return JsonResponse({
+                    "segmented_image": encoded_image, # will be the original image if there are no annotations
+                    "predicted_annotations": predicted_points,  # will be [] if no annotations
+                })
+
+            ## MedSAM
+            elif algorithm == "medsam":
+                # Optional: move import here to avoid unnecessary loading
+                from data.medsam.MedSAM_Inference import run_medsam
+
+                output_path = os.path.join(settings.MEDIA_ROOT, "medsam_outputs")
+                os.makedirs(output_path, exist_ok=True)
+
+                # You can modify run_medsam_inference to return mask_path
+                mask_path = run_medsam(image_path, output_path)
+
+                return JsonResponse({
+                    "status": "success",
+                    "output_mask": mask_path
+                })
 
         except Exception as e:
             import traceback
